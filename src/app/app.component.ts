a14b07af01b2494c748bed0c91e2ee36d390ab66
nateUX <nathan.mitchell@ni.com>
2018-04-22 21:15:22 -0500

    Adding sliders and getting them connected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import { MatSlider } from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  preserveWhitespaces: false,
 })
 export class AppComponent {
   title = 'Body Calc';
@@ -15,6 +18,28 @@ export class AppComponent {
   // showTicks = false;
   // step = 1;
   // thumbLabel = false;
-  // value = 0;
+  value = 0;
+  // Body Fat Parameters
+  bfVal = 22.4;
+  bfMin = 10;
+  bfMax = 30;
+  step = .1;
+  thumbLabel = true;
+  // Lean Mass Value
+  lmVal = 77.6;
+  lmMin = 70;
+  lmMax = 90;
+  // Bone Mass Value
+  bmVal = 0;
+  bmMin = 0;
+  bmMax = 5;
   // vertical = false;
+  onLeanInputChange(event: any) {
+    this.lmVal = event.value;
+    this.bfVal = 100 - this.lmVal;
+  }
+  onBodyInputChange(event: any) {
+    this.bfVal = event.value;
+    this.lmVal = 100 - this.bfVal;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
 
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -15,7 +18,10 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
     MatToolbarModule,
+    MatInputModule,
     MatSliderModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
